7c961c21aaee4213a71d32ceadd627a623b23bda
ArslanYM <arsalanyaqmalik@gmail.com>
2023-03-20 23:33:24 +0530

    errors fix

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -4,12 +4,12 @@ const Popup = ({correctLetters, wrongLetters, selectedWord , setPlayable}) => {
     let finalMessage = '';
     let finalMessageRevealWord = '';
     let playable = true;
-    
+
     return (
         <div class="popup-container" id="popup-container">
             <div class="popup">
-                <h2 >{finalMessage</h2>
-                <h3 >{finalMessageRevealWord</h3>
+                <h2 >{finalMessage}</h2>
+                <h3 >{finalMessageRevealWord}</h3>
                 <button id="play-button">Play Again</button>
             </div>
         </div>
