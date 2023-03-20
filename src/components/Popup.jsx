f80594ab504677e59eddae3d606903ddf66bae4b
ArslanYM <arsalanyaqmalik@gmail.com>
2023-03-20 23:39:44 +0530

    popup

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React , {useEffect} from 'react'
+import { checkWin } from '../helpers/helpers';
 
-const Popup = ({correctLetters, wrongLetters, selectedWord , setPlayable}) => {
+const Popup = ({ correctLetters, wrongLetters, selectedWord, setPlayable }) => {
     let finalMessage = '';
     let finalMessageRevealWord = '';
     let playable = true;
 
-    return (
-        <div class="popup-container" id="popup-container">
-            <div class="popup">
-                <h2 >{finalMessage}</h2>
-                <h3 >{finalMessageRevealWord}</h3>
-                <button id="play-button">Play Again</button>
-            </div>
+    useEffect(() => setPlayable(playable));
+
+    if (checkWin(correctLetters, wrongLetters, selectedWord) === 'win') {
+        finalMessage = 'Congratulations! You won! 😃';
+        playable = false;
+    } else if (checkWin(correctLetters, wrongLetters, selectedWord) === 'lose') {
+        finalMessage = 'Unfortunately you lost. 😕';
+        finalMessageRevealWord = `...the word was: ${selectedWord}`;
+        playable = false;
+    }
+
+return (
+    <div class="popup-container" id="popup-container" style={finalMessage !== '' ? {display:'flex'} : {}}>
+        <div class="popup">
+            <h2 >{finalMessage}</h2>
+            <h3 >{finalMessageRevealWord}</h3>
+            <button >Play Again</button>
         </div>
-    )
+    </div>
+)
 }
 
 export default Popup
